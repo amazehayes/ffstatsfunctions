c7d6e0f39ce5fba40b53630ce0b66575697e42ed
Addison Hayes <30516708+amazehayes@users.noreply.github.com>
2019-08-13 12:47:31 -0400

    Update yearly_plot.R

--- R/yearly_plot.R
+++ R/yearly_plot.R
@@ -1,66 +1,67 @@
-#' @title Yearly Plot ggplot
+#' @title Yearly Plot
 #'
 #' @description creates yearly plot
 #'
-#' @param pA
-#' @param pB
-#' @param pC
+#' @param players
 #' @param years
 #' @param scoring
 #'
 #' @return NULL
 #'
-#' @examples yearly_comp_player_ggplot2(pA=NULL,pB=NULL,pC=NULL,years,scoring)
+#' @examples yearly_player_chart <- function(players,years,scoring)
 #'
 #' @export
 
-yearly_comp_player_ggplot2 <- function(pA=NULL,pB=NULL,pC=NULL,years,scoring){
-  all_players <- c(pA,pB,pC)
-  tt_p = create_title(pA,pB,pC)
-
+yearly_player_chart <- function(players,years,scoring){
+  players <- c(players)
+  years <- years
+  scoring <- scoring
+  tt_p = paste("Yearly Finishes for", paste(players, collapse = " & "))
+  
+  df <- yearly_player_df(players,years)
+  df <- df %>% select(year,player,position,scoring,paste0(scoring,"_rank"))
+  mround <- function(x,base){ 
+    base*round(x/base)
+  }
+  pmax <- mround(max(df[,5],na.rm = TRUE),5) + 5
   colors = RColorBrewer::brewer.pal(3,"Set1")
-  colors = colors[1:length(all_players)]
+  colors = colors[1:length(players)]
   # requires "ggthemer
-  total_df <- yearly_comp_player_df(pA,pB,pC,years,scoring)
   ggthemr::ggthemr("dust")
-  s = ggplot(total_df,aes(x=Year,y=Rank,color=Player2,label=points_total)) +
-    geom_point() +
+  s = ggplot(df,aes(x=year,y=get(names(df)[5]),color=player)) + 
+    geom_point() + 
     geom_line() +
     theme(title = element_text(size=14),
-          axis.title=element_text(size=12),
-          legend.title=element_blank(),legend.position='bottom') +
-    scale_y_reverse(limits=c(25,0),breaks=seq(25,0,-3)) +
-    scale_color_manual(values = colors) +
-    labs(y='Position Finish',title = tt_p) +
-    annotate(
-      geom = "text",
-      x = min(total_df$Year) + 1,
-      y = 23.5,
-      label = 'FFSTATISTICS',
-      color = 'red',
-      fontface = 'bold',
-      size = 5,
-      alpha = 0.5,
-      family = 'Arial'
-    )
-
+          axis.title=element_text(size=20),
+          legend.title=element_blank()) + 
+    scale_y_reverse(limits=c(pmax,0)) + 
+    scale_color_manual(values = colors) + 
+    labs(y='Position Finish',title = tt_p,x="Year")
+  
   g <- plotly_build(s)
-
-  len <- length(all_players)
-  for(i in 1:len){
+  
+  for(i in 1:length(players)){
     p_name <- g$x$data[[i]]$name
-    if(!(p_name %in% total_df$Player2)){
+    if(!(p_name %in% df$player)){
       next
     }
-    dat <- total_df %>% filter(Player2 == p_name)
-    g$x$data[[i]]$text <- paste("Player Name:", dat$Player2, "<br>",
-                                "Rank:", dat$Rank, "<br>",
-                                "Year:", dat$Year, "<br>",
-                                "Fantasy Points Total:", dat$points_total)
+    dat <- df %>% filter(player == p_name)
+    g$x$data[[i]]$text <- paste("Player Name:", dat$player, "<br>",
+                                "Rank:", as.vector(unlist(dat[,5])), "<br>",
+                                "Year:", dat$year, "<br>",
+                                "Fantasy Points Total:", as.vector(unlist(dat[,4])))
   }
-
-  final_prod <- g %>% layout(legend = list(orientation = "h",
-                                           xanchor = 'center', # show entries horizontally
-                                           x=0.5, y=0.15))
+  
+  final_prod <- g %>% layout(images = list(source =  "https://raw.githubusercontent.com/amazehayes/ffstats_navbar/master/www/FFStats_BlackLogo_3.5x3.5.png",
+                                           xref = "paper",
+                                           yref = "paper",
+                                           x = 0,
+                                           y = 1,
+                                           sizex = 1,
+                                           sizey = 1,
+                                           opacity = 0.1,
+                                           layer = "below"),
+                             legend = list(orientation = "h", xanchor = "center", x = 0.5, y = -0.5)) %>% 
+    config(displayModeBar = FALSE)
   return(final_prod)
 }
